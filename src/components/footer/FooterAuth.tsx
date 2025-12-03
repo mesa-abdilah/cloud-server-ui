39646397cd58a5718be0772806e4393d224c9a9c
mesaabdilah1 <mesaabdilah1@gmail.com>
2025-12-03 16:41:38 +0700

    first commit

---
+++ src/components/footer/FooterAuth.tsx
@@ -0,0 +1,57 @@
+/* eslint-disable */
+
+import { Flex, Link, List, ListItem, Text, useColorModeValue } from '@chakra-ui/react';
+
+export default function Footer() {
+	let textColor = useColorModeValue('gray.400', 'white');
+	let linkColor = useColorModeValue({ base: 'gray.400', lg: 'white' }, 'white');
+	return (
+		<Flex
+			zIndex='3'
+			flexDirection={{
+				base: 'column',
+				lg: 'row'
+			}}
+			alignItems={{
+				base: 'center',
+				xl: 'start'
+			}}
+			justifyContent='space-between'
+			px={{ base: '30px', md: '0px' }}
+			pb='30px'>
+			<Text
+				color={textColor}
+				textAlign={{
+					base: 'center',
+					xl: 'start'
+				}}
+				mb={{ base: '20px', lg: '0px' }}>
+				{' '}
+				&copy; {new Date().getFullYear()}
+				<Text as='span' fontWeight='500' ms='4px'>
+					Codan Cloud. All Rights Reserved.
+				</Text>
+			</Text>
+			<List display='flex'>
+				<ListItem
+					me={{
+						base: '20px',
+						md: '44px'
+					}}>
+					<Link fontWeight='500' color={linkColor} href='mailto:admin@codan.id'>
+						Support
+					</Link>
+				</ListItem>
+				<ListItem
+					me={{
+						base: '20px',
+						md: '44px'
+					}}>
+					<Link fontWeight='500' color={linkColor} href='https://codan.id/terms-of-service'>
+						Terms of Use
+					</Link>
+				</ListItem>
+			</List>
+		</Flex>
+	);
+}
